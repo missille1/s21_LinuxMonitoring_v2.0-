c0f9998b5feede562679b3952f2f0e9d7a8dbd6e
ramonagr <ramonagr@student.21-school.ru>
2025-09-08 21:55:18 +0300

    3 add util

--- src/03/main.sh
+++ src/03/main.sh
@@ -4,11 +4,12 @@ source ./util.sh
 source ./args.sh
 source ./core.sh
 
+set -u
+
 parse_args "$@" || exit 2
 
 case "$ARG_MODE" in
 	1) delete_by_log "$ARG_LOG" ;;
 	2) delete_by_time "$ARG_T_START" "$ARG_T_END" ;;
-	3) delet_by_mask "$ARG_MASK" ;;
-	*) die "Неизвестный режим: $ARG_MODE" ;;
+	3) delet_by_mask "$ARG_MASK"
 esac
