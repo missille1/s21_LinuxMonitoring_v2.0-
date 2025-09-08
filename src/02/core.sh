3ac4fa6c6ffcbb613b6087b95886d52ae94dbb0c
Крохмалев Константин Олегович <konstantin.krohmalev@nami.ru>
2025-09-08 12:07:15 +0300

    fix 02

--- src/02/core.sh
+++ src/02/core.sh
@@ -123,6 +123,6 @@ run_core() {
 	dur=$((end_ts - start_ts))
 	dur_hms="$(fmt_dur_hms "$dur")"
 
-	printf "Начало: %s\nКонец:	%s\nВыполнение: %s\n" "$start_at" "$end_at" "$dur_hms"\
-	\n[ok] Создано. Лог: %s\n" "$log | tee -a "$log"
+	printf "Начало: %s\nКонец:\t%s\nВыполнение: %s\n[ok] Создано. Лог: %s\n" \
+	"$start_at" "$end_at" "$dur_hms" "$log" | tee -a "$log"
 }
