c5fa24c56a9bf08bd4a15e16fe6ff5d09e692ec9
Крохмалев Константин Олегович <konstantin.krohmalev@nami.ru>
2025-09-17 00:32:35 +0300

    fix 5

--- src/06/main.sh
+++ src/06/main.sh
@@ -9,7 +9,6 @@ main() {
 goaccess nginx_access_*.log \
     --log-format=COMBINED \
     --real-time-html \
-    --addr=127.0.0.1 \
     --port=7890 \
     --daemonize
   echo "[INFO] GoAccess запущен."
